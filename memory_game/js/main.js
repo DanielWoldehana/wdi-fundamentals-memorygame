b2529d3ea548afbeb87425908b461dd70859f556
Daniel Woldehana <daniel.woldehana@yahoo.com>
2019-03-10 15:13:47 -0400

    updated main.js if..else

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,30 @@
-console.log("up and running!!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("user flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User Flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User Flipped " + cardTwo);
+/*
+var cardThree = cards[2];
+cardsInPlay.push(cardThree);
+console.log("User Flipped " + cardThree);
+
+var cardFour = cards[3];
+cardsInPlay.push(cardFour);
+console.log("User Flipped " + cardFour);
+*/
+
+if (cardsInPlay.length === 2){
+    console.log();
+}
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+}
