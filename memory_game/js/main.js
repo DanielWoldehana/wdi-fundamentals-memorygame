8c461eaab2d4f7db23a353e681a7a2a46d8e850f
Daniel Woldehana <daniel.woldehana@yahoo.com>
2019-03-11 08:02:43 -0400

    added new Game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,30 +1,21 @@
+
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User Flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User Flipped " + cardTwo);
-/*
-var cardThree = cards[2];
-cardsInPlay.push(cardThree);
-console.log("User Flipped " + cardThree);
-
-var cardFour = cards[3];
-cardsInPlay.push(cardFour);
-console.log("User Flipped " + cardFour);
-*/
-
-if (cardsInPlay.length === 2){
-    console.log();
-}
-
+var checkForMatch = function() {
 if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-} else {
-    alert("Sorry, try again.");
-}
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
+};
+
+var flipCard = function(cardId) {
+    //console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    return "User flipped " + cards[cardId];
+};
+console.log(flipCard(0));
+console.log(flipCard(2));
+console.log(checkForMatch());
